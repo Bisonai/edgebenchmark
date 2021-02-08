89029d93c2ce12a2f3d5b81f4858989ca2bd9f65
Martin Kersner <m.kersner@gmail.com>
2021-02-08 21:40:57 +0900

    Bump up package version to 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="edgebenchmark",
-    version="0.0.7",
+    version="0.0.8",
     author="Bisonai",
     author_email="contact@bisonai.com",
     description="Tool to benchmark speed of machine learning models on real mobile devices",
